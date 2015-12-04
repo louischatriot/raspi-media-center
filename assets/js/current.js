f7ae942708b50b596210ba8feb220ef79ea4694d
Louis Chatriot <louis.chatriot@gmail.com>
2015-12-04 17:05:08 +0100

    Added socket.io

--- assets/js/current.js
+++ assets/js/current.js
@@ -9,21 +9,6 @@ $('#stop').on('click', function () {
   $.ajax({ type: 'GET', url: '/api/stop' });
 });
 
-$('#small-forward').on('click', function () {
-  $.ajax({ type: 'GET', url: '/api/small-forward' });
-});
-
-$('#small-backward').on('click', function () {
-  $.ajax({ type: 'GET', url: '/api/small-backward' });
-});
-
-$('#big-forward').on('click', function () {
-  $.ajax({ type: 'GET', url: '/api/big-forward' });
-});
-
-$('#big-backward').on('click', function () {
-  $.ajax({ type: 'GET', url: '/api/big-backward' });
-});
 
 
 function niceTimeFormat (s) {
